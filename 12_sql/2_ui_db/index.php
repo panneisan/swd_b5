443764e32978b58f37af33d5b6d3df218ebcafd3
heinhtetzan <mmsonestopit@gmail.com>
2020-02-11 14:03:58 +0630

    now

--- 12_sql/2_ui_db/index.php
+++ 12_sql/2_ui_db/index.php
@@ -1,13 +1,6 @@
 <?php include 'template/header.php'; ?>
 
-<button class="btn btn-primary">
-    <i class="fa fa-car"></i>
-    Hello New Car
-</button>
 
-<a href="<?php echo URL."test.php" ?>" class="btn btn-secondary">To Test</a>
-
-<img src="<?php assets('img/two_bg.jpg') ?>" class="w-100" alt="">
 
 
 <?php include 'template/footer.php'; ?>
